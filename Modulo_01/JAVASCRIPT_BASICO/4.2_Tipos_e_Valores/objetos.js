3e3a76040a87c5472b1d62a9f92367a45039b652
EdiJunior88 <er.moura88@yahoo.com.br>
2021-03-10 20:26:38 -0300

    Módulo 01 - JavaScript Básico

---
+++ Modulo_01/JAVASCRIPT_BASICO/4.2_Tipos_e_Valores/objetos.js
@@ -0,0 +1,23 @@
+//Objetos
+
+var aluno1 = {
+  matricula: 7627364,
+  nome: 'Danilo Ferreira',
+  curso: 'Bootcamp Front End',
+  ativo: true
+};
+
+console.log(aluno1.nome);
+
+aluno1.dataNascimento = '03/07/1985'
+
+console.log(aluno1.dataNascimento);
+
+delete aluno1.dataNascimento;
+
+var x;
+var y = null;
+
+//Arrays
+
+var frutas = ['Banana', 'Laranja', 'Maçã'];
---
+++ Modulo_01/JAVASCRIPT_BASICO/4.2_Tipos_e_Valores/primitivos.js
@@ -0,0 +1,5 @@
+// Tipos primitivos
+
+var vNumber = 5.78;
+var vString = 'abacaxi';
+var vBoolean = true;
