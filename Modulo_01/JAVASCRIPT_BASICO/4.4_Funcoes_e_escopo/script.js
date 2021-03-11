7e4c7df6c4e067d13b61e638228a6b733c9968be
EdiJunior88 <er.moura88@yahoo.com.br>
2021-03-10 21:09:56 -0300

    Módulo 01 - JavaScript Básico

---
+++ Modulo_01/JAVASCRIPT_BASICO/4.4_Funcoes_e_escopo/script.js
@@ -0,0 +1,17 @@
+
+var m = 'Olá';
+imprimeOla();
+
+function imprimeOla(){
+  var nome = 'Danilo';
+  console.log(m);
+  console.log(nome);
+  imprimeOla2();
+
+  function imprimeOla2(){
+    console.log(m);
+    console.log(nome);
+  }  
+}
+
+
